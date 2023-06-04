4cec22679b9df4a45753bcf5917da7d21f553e04
Константин Шарыгин <kot.074@mail.ru>
2023-06-04 16:39:56 +0300

    Добавлена информация о текущем пользователе в шапку. Добавлена возможность "разлогиниться".
    Signed-off-by: Константин Шарыгин <kot.074@mail.ru>

---
+++ src/components/Header/CurrentUser/CurrentUser.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './CurrentUser.module.css'
+import {getCurrentUser} from "../../../Api/UsersApi";
+import {useNavigate} from "react-router-dom";
+
+const CurrentUser = () => {
+    const currentUser = getCurrentUser();
+    const navigate = useNavigate();
+
+    const onExit = () => {
+        localStorage.removeItem('currentUser');
+        navigate(0);
+    }
+    const toProfile = () => {
+        navigate(`/users/edit/${currentUser.id}`);
+    }
+
+    if (currentUser.token === '') {
+        return <></>
+    }
+
+    return (
+        <div className={styles.block}>
+            <div className={styles.avatar}>
+                <img alt={'avatar'} src={'/unknownAvatar.jpeg'}/>
+            </div>
+            <div className={styles.user}>
+                <span onClick={toProfile}>{currentUser.user ?? ''}</span>
+            </div>
+            <div className={styles.logout}>
+                <span onClick={onExit}>Выйти</span>
+            </div>
+        </div>
+    )
+}
+
+export default CurrentUser;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from './Header.module.css';
 import Logo from "./Logo/Logo";
 import Text from "./Text/Text";
+import CurrentUser from "./CurrentUser/CurrentUser";
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <Logo />
             <Text />
+            <CurrentUser />
         </header>
     );
 }
